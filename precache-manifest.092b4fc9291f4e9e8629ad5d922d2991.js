a91543ea05ecdd65b7a13c1d6c666d37409db2e6
fabemiliano <fab.emiliano@gmail.com>
2020-07-27 13:34:00 -0300

    Updates

---
+++ precache-manifest.092b4fc9291f4e9e8629ad5d922d2991.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c462a80a95b72967c2bdf645307907da",
+    "url": "/exchange-converter/index.html"
+  },
+  {
+    "revision": "eaa85cd26a83e14308ce",
+    "url": "/exchange-converter/static/css/main.d44f39f1.chunk.css"
+  },
+  {
+    "revision": "faf98dc8917db2d9a047",
+    "url": "/exchange-converter/static/js/2.1191c29a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exchange-converter/static/js/2.1191c29a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaa85cd26a83e14308ce",
+    "url": "/exchange-converter/static/js/main.9df44248.chunk.js"
+  },
+  {
+    "revision": "7f4d892ca1ac847dd2a5",
+    "url": "/exchange-converter/static/js/runtime-main.950cf470.js"
+  },
+  {
+    "revision": "5df55aa2a1abd6174f63ae8b50b0bbc9",
+    "url": "/exchange-converter/static/media/arrow.5df55aa2.svg"
+  }
+]);
